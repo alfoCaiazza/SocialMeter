76a724998a67cf2d250b08fa890f272f9936206f
alfoCaiazza <acaiazza2000@gmail.com>
2023-12-14 16:40:44 +0100

    Added 'results.js' template to folder 'components'

---
+++ client/src/components/Results.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className='container-fluid d-flex flex-column min-vh-100 p-0'>
+      <div className="text-center mt-5">
+        <h2 className='display-4' style={{marginTop: '7%', color: '#171717'}}><strong>Qui saranno mostrati i grafici</strong></h2>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
