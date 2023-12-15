2febc2bd14f8c4096eeb49af8b53165a600e2ba6
alfoCaiazza <acaiazza2000@gmail.com>
2023-12-15 16:42:26 +0100

    Changed css of cards

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -4,9 +4,11 @@ import React from 'react';
 const Card = ({ post }) => {
   return (
     <div className="card mb-3" style={{borderRadius: '10px'}}>
+      <div className='card-header'>
+        <h5 className="card-title mb-2 lead">{post.title}</h5>
+        <h6 className='card-subtitle lead' style={{fontSize: 'medium'}}>Autore: {post.author} - Data: {post.pubdate}</h6>
+      </div>
       <div className="card-body">
-        <h5 className="card-title mb-2">{post.title}</h5>
-        <h6 className='card-subtitle mb-2 text-body-secondary' >Autore: {post.author} - Data: {post.pubdate}</h6>
         <p className="card-text">{post.text}</p>
         <div className='information'>
           <span>
