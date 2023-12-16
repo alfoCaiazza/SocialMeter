29f64510532286e1797d37978709e14e16491276
alfoCaiazza <acaiazza2000@gmail.com>
2023-12-16 18:19:31 +0100

    Changed text

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div className='container-fluid d-flex flex-column min-vh-100 p-0'>
       <div className="text-center mt-5">
-        <h2 className='display-4' style={{marginTop: '7%', color: '#171717'}}><strong>Qui saranno mostrati i grafici</strong></h2>
+        <h2 className='display-4' style={{marginTop: '7%', color: '#171717'}}><strong>Qui saranno mostrati i risultati</strong></h2>
       </div>
     </div>
   );
