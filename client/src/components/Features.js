f6f1a5c8f35d37e248b5533ee421a248c49fcfe7
alfoCaiazza <acaiazza2000@gmail.com>
2023-12-16 12:20:22 +0100

    Added logo to footer

--- client/src/components/Features.js
+++ client/src/components/Features.js
@@ -12,11 +12,11 @@ const HomePage = () => {
 
   return (
     <div className='container-fluid d-flex flex-column min-vh-100 p-0 features-background'>
-      <div className="text-center mt-5">
+      <div className="text-center position-absolute top-50 start-50 translate-middle mt-5">
         <h2 className='display-4' style={{marginTop: '7%', color: '#171717'}}><strong>Features</strong></h2>
-        <ul className="list-unstyled" style={{marginTop: '2%'}}>
+        <ul className="list-unstyled" style={{marginTop: '5%'}}>
           {links.map((link, index) => (
-            <li key={index} style={{marginBottom: '1%'}}>
+            <li key={index} style={{marginBottom: '5%'}}>
               <Link to={link.to} className="my-link">
                 {link.label}
               </Link>
