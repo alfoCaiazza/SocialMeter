0f0dab56854ed643995bf32fd39513d0624f92af
alfoCaiazza <acaiazza2000@gmail.com>
2024-01-01 17:59:07 +0100

    Updated the function of scraping data: the scraping method has been changed in search() and use now the query parameter for filtring posts

--- server/scraping_reddit_data.py
+++ server/scraping_reddit_data.py
@@ -7,6 +7,7 @@ import praw
 import os
 import logging
 import pandas as pd
+import time
 
 def setup_logging():
     logging.basicConfig(
@@ -43,55 +44,51 @@ def save_existing_df(existing_df, file_path="existing_posts.csv"):
 #Cambia il limit successivamente
 def scrape_reddit(subreddit, limit=None, existing_df=None, keywords=None, max_posts=None):
     titles, texts, scores, ids, pub_dates, posts_data = [], [], [], [], [], []
-    tot_posts = 0
-    last_id = None
+    query = ' OR '.join(keywords)
 
-    while tot_posts < max_posts:
-        for iteration, submission in enumerate(subreddit.hot(limit=limit, params={'after': last_id})):
-            logging.info(f"Processing post {iteration + 1}/{limit}")
-            tot_posts += 1
-            last_id = submission.id
+    for iteration, submission in enumerate(subreddit.search(query, limit=limit)):
+        logging.info(f"Processing post {iteration + 1}/{limit}")
 
-            # Check for kewywords in the title or text
-            contains_keyword = any(keyword.lower() in submission.title.lower() or
-                                keyword.lower() in submission.selftext.lower()
-                                for keyword in keywords) if keywords else True
-            
-            print(f"Post ID: {submission.id}, Contains Keyword: {contains_keyword}")
+        # Check for kewywords in the title or text
+        contains_keyword = any(keyword.lower() in submission.title.lower() or
+                            keyword.lower() in submission.selftext.lower()
+                            for keyword in keywords) if keywords else True
+        
+        print(f"Post ID: {submission.id}, Contains Keyword: {contains_keyword}")
 
-            # Skip to the next iteration if keywords are specified and not present
-            if keywords and not contains_keyword:
-                print("Skipping post due to missing keywords.")
-                continue
+        # Skip to the next iteration if keywords are specified and not present
+        if keywords and not contains_keyword:
+            print("Skipping post due to missing keywords.")
+            continue
 
-            # Check for duplicate submissions using existing_df
-            if existing_df is None or submission.id not in existing_df["id"].values:
-                titles.append(submission.title)
-                texts.append(submission.selftext)
-                scores.append(submission.score)
-                ids.append(submission.id)
-                pub_dates.append(datetime.utcfromtimestamp(submission.created_utc))
+        # Check for duplicate submissions using existing_df
+        if existing_df is None or submission.id not in existing_df["id"].values:
+            titles.append(submission.title)
+            texts.append(submission.selftext)
+            scores.append(submission.score)
+            ids.append(submission.id)
+            pub_dates.append(datetime.utcfromtimestamp(submission.created_utc))
 
-                # Retrieve comments for the current submission
-                submission.comments.replace_more(limit=None)
-                comments_data = []
-                for comment in submission.comments.list():
-                    comment_data = {
-                        "score": comment.score,
-                        "text": comment.body
-                    }
-                    comments_data.append(comment_data)
-
-                post_data = {
-                    "title": submission.title,
-                    "text": submission.selftext,
-                    "score": submission.score,
-                    "id": submission.id,
-                    "pub_date": datetime.utcfromtimestamp(submission.created_utc),
-                    "comments": comments_data
+            # Retrieve comments for the current submission
+            time.sleep(2)
+            submission.comments.replace_more(limit=None)
+            comments_data = []
+            for comment in submission.comments.list():
+                comment_data = {
+                    "score": comment.score,
+                    "text": comment.body
                 }
+                comments_data.append(comment_data)
 
-                posts_data.append(post_data) 
+            post_data = {
+                "title": submission.title,
+                "text": submission.selftext,
+                "score": submission.score,
+                "id": submission.id,
+                "pub_date": datetime.utcfromtimestamp(submission.created_utc),
+                "comments": comments_data
+            }
+            posts_data.append(post_data) 
 
     # Creazione di un DataFrame per gestire gli ID
     new_df = pd.DataFrame({"id": ids})
@@ -104,7 +101,8 @@ def main():
 
     targeted_subreddits = ["italy", "italia", "napoli", "milano", "torino", "venezia", "oknotizie"]
     keywords = ["Femminicidio", "Molestie", "Stupro", "MeToo","Femminismo",
-                "GenderEquality", "Sessismo", "Misoginia", "Maschilismo"]
+                "GenderEquality", "Sessismo", "Misoginia", "Maschilismo",
+                "Giulia", "Violenza", "Donna", "Donne"]
 
     reddit = praw.Reddit(
         client_id=os.getenv('REDDIT_CLIENT_ID'),
@@ -121,7 +119,7 @@ def main():
         subreddit = reddit.subreddit(sub)
         logging.info(f"Processing subreddit: {subreddit.display_name}")
 
-        titles, texts, scores, ids, pub_dates, posts_data, new_df = scrape_reddit(subreddit, existing_df=existing_df, keywords=keywords, max_posts=2000)
+        titles, texts, scores, ids, pub_dates, posts_data, new_df = scrape_reddit(subreddit, limit=10, existing_df=existing_df, keywords=keywords)
 
         #Verifica se la lista dei post recuperati è non vuota
         if posts_data: 
