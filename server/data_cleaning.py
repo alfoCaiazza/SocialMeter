757fac9a17fe5236abb851b88c7ef0d8667aebdf
alfoCaiazza <acaiazza2000@gmail.com>
2024-01-02 11:42:24 +0100

    Added the data cleaning features

--- server/data_cleaning.py
+++ server/data_cleaning.py
@@ -1,26 +1,29 @@
 from scraping_reddit_data import connect_to_mongo, setup_logging
-from pymongo import MongoClient
 from dotenv import load_dotenv
 import logging
 import re
 import emoji
+import string
 
 
 def clean_text(text):
     #Eliminazione dei caratteri speciali e degli spazi in eccesso
-    cleaned_text = re.sub(r'[^\w\s]', '', text).strip()
+    cleaned_text = re.sub(r'[^\w\s]', ' ', text).strip()
 
     #Eliminazione delle emoji
     cleaned_text = emoji.demojize(cleaned_text)
 
     #Eliminazione dei link
-    cleaned_text = re.sub(r'http\S+', '', cleaned_text)
+    cleaned_text = re.sub(r'http\S+', ' ', cleaned_text)
 
-    #Eliminazione del tag "[meta]"
-    cleaned_text = cleaned_text.replace('[meta]', '')
+    #Eliminazione del tag "[meta]" --> devono essere rimossi direttamente i post e i commenti non solo il tag
+    #cleaned_text = cleaned_text.replace('[meta]', '')
 
     #Eliminazione degli spazi multipli
     cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
+
+    #Resa delle parole in minuscolo
+    cleaned_text = cleaned_text.lower()
 
     return cleaned_text
 
@@ -34,8 +37,7 @@ def main():
     clean_reddit_posts = db['cleanedPosts']
 
     for post in reddit_posts:
-        post_text = post['text']
-        cleaned_text = clean_text(post_text)
+        cleaned_text = clean_text(post['text'])
 
         post_comments = post.get('comments', [])
         cleaned_comments = []
