411af397587ecc10dc82c3207d150eb7bb17b599
alfoCaiazza <acaiazza2000@gmail.com>
2024-01-16 10:16:46 +0100

    Generalized component

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,21 +1,13 @@
-import { useNavigate } from 'react-router-dom';
-
-const Card = ({ title, category, image, bootstrapIcon }) => {
-  const navigate = useNavigate();
-
-  const onCardClick = () => {
-      navigate(`/trends/${category}`);
-  };
-
+const Card = ({ title, image, onCardClick }) => {
   return (
-    <div className='col' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '200px' }}>
-      <div className="card" style={{width: '150px'}} onClick={onCardClick}>
-        <img src={image} alt={category} style={{maxWidth: '80%', margin:'auto' }} />
-        <div className='card-body'>
-          <h6>{title}</h6>
-        </div>
+      <div className='col' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '200px' }}>
+          <div className="card" style={{width: '150px'}} onClick={onCardClick}>
+              <img src={image} alt={title} style={{maxWidth: '80%', margin:'auto' }} />
+              <div className='card-body'>
+                  <h6>{title}</h6>
+              </div>
+          </div>
       </div>
-    </div>
   );
 };
 
