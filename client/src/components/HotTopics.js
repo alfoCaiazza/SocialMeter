c538d50c4b76d8d57a0589fa554d79ccd19aaf7d
alfoCaiazza <acaiazza2000@gmail.com>
2024-01-18 11:20:48 +0100

    Corrected component writing mistake

--- client/src/components/HotTopics.js
+++ client/src/components/HotTopics.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import WordCloudComponent from './WordCloundComponent';
+import WordCloudComponent from './WordCloudComponent';
 
 const HotTopics = () =>{
     const [data, setData] = useState(null)
