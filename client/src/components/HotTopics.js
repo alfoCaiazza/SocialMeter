f75466c4eb8a441227e6f028c194ff33c53f4419
alfoCaiazza <acaiazza2000@gmail.com>
2024-01-26 22:41:17 +0100

    Added a text header

--- client/src/components/HotTopics.js
+++ client/src/components/HotTopics.js
@@ -44,7 +44,10 @@ const HotTopics = () =>{
     return (
         <div className='container-fluid d-flex flex-column min-vh-100 p-0'>
             <div className='text-center' style={{marginTop: "3%"}}>
-                <h2 className='display-6' style={{marginTop: '1%', color: '#171717'}}><strong>Parole Chiavi per la tematica {getCategoryString(category)}</strong></h2>
+                <h2 className='display-6' style={{marginTop: '1%', color: '#171717'}}><strong>{getCategoryString(category)}</strong></h2>
+                <p style={{ fontSize: '1.5rem' }}>
+                    I concetti e i termini più rilevanti all'interno dei post che trattano di {getCategoryString(category)} sono i seguenti
+                </p>
             </div>
             <div className='text-center position-absolute top-50 start-50 translate-middle mt-5 wordCloudContainer'>
                 {data ? <WordCloudComponent words={data} options={options}/> : <p>Caricamento dati...</p>}
