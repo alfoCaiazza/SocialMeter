d3fcf0b2067be88ca49b3a1377e5d84bfe41957a
alfoCaiazza <acaiazza2000@gmail.com>
2024-01-26 22:40:28 +0100

    Changed collection name to 'analisedPosts'

--- server/filtered_posts_module.py
+++ server/filtered_posts_module.py
@@ -7,21 +7,12 @@ def get_filtered_posts(app, db):
     def inner_get_filtered_posts():
         try:
             category = request.args.get('post_category')
-            posts = db["dataAnalysis"].find({'category' : category})
-            result = [{
-                'category' : post['category'],
-                'id': post['id'],
-                'title': post['title'],
-                'text' : post['text'],
-                'date': post['pub_date'],
-                'year' : post['year'],
-                'month' : post['month'],
-                'day' : post['day'],
-                'postivity': post['positivity'],
-                'neutrality' : post['neutrality'],
-                'negativity' : post['negativity'],
-                'sentiment' : post['sentiment']
-            } for post in posts]
+            posts = db["analisedPosts"].find({'category' : category}).sort('pub_date', -1)
+            result = []
+            for post in posts:
+                post_dict = dict(post)
+                post_dict.pop('_id', None)
+                result.append(post_dict)
 
             return jsonify(result)
         except Exception as e:
