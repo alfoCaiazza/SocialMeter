f659254c41e1c4e3827d5914a538a201e75b5b9f
alfoCaiazza <acaiazza2000@gmail.com>
2024-02-02 09:27:02 +0100

    Updated layout

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,56 +2,47 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    const as = [
-        { to: '/filtered_posts', label: 'Filtra Post' },
-        { to: '/hot_topics_category', label: 'Temi Caldi' },
-        { to: '/trends_category', label: 'Tendenze' },
-        { to: '/matrix_profile', label: 'Evoluzione Interazioni' },
-      ];
-    const logo = require('./images/Black.png');
-  
+    return (
+        <footer id="footer">
+            <div className="footer-top">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-3 col-md-6 footer-contact">
+                            <h3>SocialMeter</h3>
+                        </div>
 
-  return (
-    <footer id="footer">
-        <div className="footer-top">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-3 col-md-6 footer-contact">
-                        <h3>SocialMeter</h3>
-                    </div>
+                        <div className="col-lg-3 col-md-6 footer-links">
+                            <h4>Link</h4>
+                            <ul>
+                            <li><i className="bx bx-chevron-right"></i> <Link to="/">Home</Link></li>
+                            <li><i className="bx bx-chevron-right"></i> <Link to="/about_us">About us</Link></li>
+                            <li><i className="bx bx-chevron-right"></i> <Link to="/features">Features</Link></li>
+                            </ul>
+                        </div>
 
-                    <div className="col-lg-3 col-md-6 footer-links">
-                        <h4>Link</h4>
-                        <ul>
-                        <li><i className="bx bx-chevron-right"></i> <Link to="/">Home</Link></li>
-                        <li><i className="bx bx-chevron-right"></i> <Link to="/about_us">About us</Link></li>
-                        <li><i className="bx bx-chevron-right"></i> <Link to="/features">Features</Link></li>
-                        </ul>
-                    </div>
-
-                    <div className="col-lg-3 col-md-6 footer-links">
-                        <h4>Features</h4>
-                        <ul>
-                        <li><i className="bx bx-chevron-right"></i> <Link to="/filtered_posts_category">Esplora Post</Link></li>
-                        <li><i className="bx bx-chevron-right"></i> <Link to="/hot_topics_category">Parole Chiavi</Link></li>
-                        <li><i className="bx bx-chevron-right"></i> <Link to="/trends_category">Tendenze</Link></li>
-                        <li><i className="bx bx-chevron-right"></i> <Link to="/matrix_profile">Engagement</Link></li>
-                        </ul>
+                        <div className="col-lg-3 col-md-6 footer-links">
+                            <h4>Features</h4>
+                            <ul>
+                            <li><i className="bx bx-chevron-right"></i> <Link to="/filtered_posts_category">Esplora Post</Link></li>
+                            <li><i className="bx bx-chevron-right"></i> <Link to="/hot_topics_category">Parole Chiavi</Link></li>
+                            <li><i className="bx bx-chevron-right"></i> <Link to="/trends_category">Tendenze</Link></li>
+                            <li><i className="bx bx-chevron-right"></i> <Link to="/matrix_profile">Engagement</Link></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div className="container py-4">
-            <div className="copyright">
-                &copy; Copyright <strong><span>SocialMeter</span></strong>. All Rights Reserved
+            <div className="container py-4">
+                <div className="copyright">
+                    &copy; Copyright <strong><span>SocialMeter</span></strong>. All Rights Reserved
+                </div>
+                <div className="credits">
+                    Designed by <Link to="https://bootstrapmade.com/">BootstrapMade</Link>
+                </div>
             </div>
-            <div className="credits">
-                Designed by <Link to="https://bootstrapmade.com/">BootstrapMade</Link>
-            </div>
-        </div>
-    </footer>
-  );
+        </footer>
+    );
 }
 
 export default Footer;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import logo from './images/logo_chatGPT.png';
 
 const Header = () => {
   const [showLink, setShowLink] = useState(true);
