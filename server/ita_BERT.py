50d8fdc026ca0fb2ed2941847b9cfe01ec729229
alfoCaiazza <acaiazza2000@gmail.com>
2024-02-04 10:41:45 +0100

    Added code comments

--- server/ita_BERT.py
+++ server/ita_BERT.py
@@ -1,17 +1,17 @@
 import torch
 
-# Funzione per classificare il sentiment
+# Function to Classify Sentiment
 def classify_sentiment(sentence, tokenizer, model):
-    # Prepara i dati di input
+    # Prepare input data
     inputs = tokenizer(sentence, return_tensors="pt", truncation=True, max_length=512)
 
-    # Ottieni le previsioni del modello
+    # Get model predictions
     outputs = model(**inputs)
     predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
 
     probability = predictions.detach().numpy()[0]
 
-    # Converte le previsioni in sentimenti
+    # Convert predictions into sentiment strings
     compound = predictions.argmax().item()
     if compound == 0:
         sentiment = "Negativo"
